5f2fe9d80baeaf6ac3a279e184163b23b05d2942
Oğuz Han Turan <turan.oguzhan@hotmail.com>
2021-11-04 00:20:56 +0300

    init example
    h2 Database for local storage with data jpa beginning example

---
+++ h2example/src/main/java/com/ouz/initialdatajpah2/InitialDataJpah2Application.java
@@ -0,0 +1,13 @@
+package com.ouz.initialdatajpah2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitialDataJpah2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitialDataJpah2Application.class, args);
+	}
+
+}
---
+++ h2example/src/test/java/com/ouz/initialdatajpah2/InitialDataJpah2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ouz.initialdatajpah2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitialDataJpah2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
